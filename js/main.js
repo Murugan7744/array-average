b399e7bf38488948162770566a0798803e941d82
Logis <you@example.commuruganmsd7710@gmail.com>
2023-05-29 15:21:45 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var a=[17,27,73,83,13,55,21,91,33,63];
+var sum=0;
+for(i=0;i<a.length;i++)
+{
+	sum+=a[i];
+	average=sum/a.length;
+}
+document.write("Average of value in array"+ " "+ "="+ " "+ average);
